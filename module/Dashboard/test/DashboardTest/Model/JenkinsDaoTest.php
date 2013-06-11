bc9b4cb53e36453124122e379a17386ac1e8834b
Wojciech Iskra <wojciech.iskra@schibsted.pl>
2013-06-11 14:52:09 +0200

    Improving code-style

--- module/Dashboard/test/DashboardTest/Model/JenkinsDaoTest.php
+++ module/Dashboard/test/DashboardTest/Model/JenkinsDaoTest.php
@@ -4,16 +4,20 @@ namespace DashboardTest\Model;
 use Dashboard\Model\JenkinsDao;
 
 class JenkinsDaoTest extends \PHPUnit_Framework_TestCase {
-
+    /**
+     * Testing proper Jenkins API URL - should return JSON
+     */
     public function testProperApiUrl() {
         $response = JenkinsDao::getInstance()->request('http://ci.vgnett.no/view/VGTV/job/VGTV_front-end/api/json?pretty=true');
 
         $this->assertTrue(is_array($response), 'Testing proper API URL');
     }
 
+    /**
+     * Testing wrong API URL - will not return JSON
+     * @expectedException \Zend\Json\Exception\RuntimeException
+     */
     public function testImproperApiUrl() {
-        $this->setExpectedException('Zend\Json\Exception\RuntimeException');
         JenkinsDao::getInstance()->request('http://ci.vgnett.no/');
     }
-
 }
