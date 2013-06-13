7f5ae9ee9244be84a91e0f38c64ce5ea7bc8b728
Konrad Turczyński <konrad.turczynski@schibsted.pl>
2013-06-13 13:39:15 +0200

    custom exception for invalid widget type

---
+++ module/Dashboard/src/Dashboard/Model/Widget/Exception/InvalidWidgetTypeException.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *
+ * @author Konrad Turczynski <konrad.turczynski@schibsted.pl>
+ */
+namespace Dashboard\Model\Widget\Exception;
+
+class InvalidWidgetTypeException extends \Exception {}
