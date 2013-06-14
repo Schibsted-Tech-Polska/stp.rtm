08b54d2e1465d9e0cc584f644ea81c3a30972c6b
Wojciech Iskra <wojciech.iskra@schibsted.pl>
2013-06-14 13:45:17 +0200

    Added a few unit tests for NewRelicDao and changed endpoint URLS for newrelic api

--- module/Dashboard/config/dao/newRelicDao.config.php
+++ module/Dashboard/config/dao/newRelicDao.config.php
@@ -6,9 +6,8 @@
 return array(
     'urls' => array(
         'fetchRpmForGraphWidget' => 'https://api.newrelic.com/api/v1/accounts/:accountId:/applications/:appId:/data.json?metrics[]=HttpDispatcher&field=requests_per_minute&begin=:beginDateTime:&end=:endDateTime:',
-        'fetchErrorRateForGraphWidget' => 'https://api.newrelic.com/api/v1/accounts/:accountId:/applications/:appId:/data.json?metrics[]=Errors/all&field=errors_per_minute&begin=:beginDateTime:&end=:endDateTime:',
+        'fetchThresholdValues' => 'https://api.newrelic.com/api/v1/accounts/:accountId:/applications/:appId:/threshold_values.xml',
         'fetchCpuUsageForGraphWidget' => 'https://api.newrelic.com/api/v1/accounts/:accountId:/applications/:appId:/data.json?metrics[]=CPU/User Time&field=percent&begin=:beginDateTime:&end=:endDateTime:',
         'fetchAverageResponseTimeForGraphWidget' => 'https://api.newrelic.com/api/v1/accounts/:accountId:/applications/:appId:/data.json?metrics[]=HttpDispatcher&field=average_response_time&begin=:beginDateTime:&end=:endDateTime:',
-        'fetchMemoryForGraphWidget' => 'https://api.newrelic.com/api/v1/accounts/:accountId:/applications/:appId:/data.json?metrics[]=Memory/Physical&field=used&begin=:beginDateTime:&end=:endDateTime:',
     ),
 );
