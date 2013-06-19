dedac63313bfc41992aa72ff4a25abc532a975de
Konrad Turczyński <konrad.turczynski@schibsted.pl>
2013-06-20 01:28:11 +0200

    Refactored javascript classes of widgets

--- public/js/widget/NumberWidget.js
+++ public/js/widget/NumberWidget.js
@@ -1,75 +1,52 @@
+function NumberWidget(widget) {
+    this.$widget = $(widget);
+
+    this.options.configName = $('.container').data('config-name');
+    this.options.widgetId = this.$widget.attr('id');
+
+    this.valueField = this.$widget.find('.value');
+    this.diffContainer = this.$widget.find('.change-rate');
+    this.arrowContainer = this.$widget.find('.change-rate i');
+
+}
+
+NumberWidget.prototype = new Widget();
+NumberWidget.prototype.constructor = NumberWidget;
+
+$.extend(NumberWidget.prototype, {
+
+    handleResponse: function (response) {
+
+        this.valueField.text(response.data);
+
+        var oldValue = this.$widget.data("oldValue");
+
+        if ($.isNumeric(oldValue) && $.isNumeric(response.data)) {
+            var diff = response.data - oldValue;
+
+            var percentageDiff = Math.round(Math.abs(diff) / oldValue * 100) + "%";
+
+            this.$widget.find(".difference").text(percentageDiff + "(" + oldValue + ") ");
+            this.$widget.find(".change-rate").show();
+
+            if (diff > 0) {
+                this.arrowContainer.attr("class", "icon-arrow-up");
+            } else {
+                this.arrowContainer.attr("class", "icon-arrow-down");
+            }
+        } else {
+            this.diffContainer.hide();
+        }
+
+        this.$widget.data("oldValue", response.data);
+    }});
+
 $(document).ready(function () {
+    var widget = $("#vgtvCpuUsage");
 
-    var NumberWidget = (function () {
+    $('.NumberWidget').each(function(){
+        new NumberWidget(this).startListening();
+    });
 
-        var $widgets = $(".NumberWidget");
-        var $config = $(".container").data('config-name');
-        var urlBase = "/resources/" + $config + "/";
 
-        var init = function () {
-                $.ajaxSetup({
-                    dataType: "json",
-                    timeout: 100000 });
-            },
-
-            startListening = function () {
-
-                init();
-
-                $widgets.each(function () {
-
-                    var oldValueHash = '';
-                    var nodeName = this.id;
-                    var widgetNode = $(this);
-                    var valueField = widgetNode.find('.value');
-
-                    (function poll() {
-                        $.ajax({
-                            complete: poll,
-                            url: urlBase + nodeName + oldValueHash,
-                            success: function (response) {
-                                oldValueHash = "/" + response.hash;
-                                valueField.text(response.data);
-
-                                updateDiff(response, widgetNode);
-
-                            }
-                        });
-                    })();
-                });
-            },
-
-            /**
-             * Updates field with difference info
-             */
-            updateDiff = function (response, widgetNode) {
-
-                var oldValue = widgetNode.data("oldValue");
-
-                if ($.isNumeric(oldValue) && $.isNumeric(response.data)) {
-                    var diff = response.data - oldValue;
-
-                    var percentageDiff = Math.round(Math.abs(diff) / oldValue * 100) + "%";
-
-                    widgetNode.find(".difference").text(percentageDiff + "(" + oldValue + ") ");
-                    widgetNode.find(".change-rate").show();
-
-                    if (diff > 0) {
-                        widgetNode.find("i").attr("class", "icon-arrow-up");
-                    } else {
-                        widgetNode.find("i").attr("class", "icon-arrow-down");
-                    }
-                } else {
-                    $(widgetNode).find(".change-rate").hide();
-                }
-
-                widgetNode.data("oldValue", response.data);
-            };
-
-        return {
-            startListening: startListening
-        };
-    })();
-
-    NumberWidget.startListening();
 });
