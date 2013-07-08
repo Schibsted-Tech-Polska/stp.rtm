ee4cf6994a8649c22d5033789031dd4597186822
tomrac87 <tomciotomcio87>
2013-07-08 16:13:16 +0200

    Moved SCSS files to a proper folder, added compass config file, added jshint config file

---
+++ scripts/compass/compass.rb
@@ -0,0 +1,23 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/css"
+sass_dir = "public/scss"
+images_dir = "public/img"
+http_generated_images_path= "../img"
+sprite_load_path = "public/img-src"
+javascripts_dir = "public/js"
+fonts_dir = "css/fonts"
+http_fonts_dir = "css/fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :scss
