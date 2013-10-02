f4eea1ad88dfc863247c07190e98e0fa6a013d04
Wojciech Iskra <wojciech.iskra@schibsted.pl>
2013-10-02 09:03:55 +0200

    Changed displaying splunk 500 errors to display number of errors occured and last error time.

--- module/Dashboard/src/Dashboard/Model/Dao/SplunkDao.php
+++ module/Dashboard/src/Dashboard/Model/Dao/SplunkDao.php
@@ -20,7 +20,13 @@ class SplunkDao extends AbstractDao {
         $splunkJson = $this->request($this->config['url'], array(), 'json', $this->config['auth'], $this->config['jobs'][$params['config']]);
 
         if ($splunkJson) {
-            return $splunkJson['columns'][0];
+            $returnArray = array();
+            foreach ($splunkJson['columns'][1] as $key => $url) {
+                $returnArray[$key]['url'] = $url;
+                $returnArray[$key]['numberOfErrors'] = $splunkJson['columns'][0][$key];
+                $returnArray[$key]['lastErrorTime'] = $splunkJson['columns'][2][$key];
+            }
+            return $returnArray;
         } else {
             return array();
         }
