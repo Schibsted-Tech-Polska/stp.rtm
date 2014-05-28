771793f8b5e71a317166b1a08b2bc7ee55dc250e
wiskra <wojciech.iskra@schibsted.pl>
2014-05-28 12:17:42 +0200

    Adding some new relic dao unit tests

--- module/Dashboard/test/unit/DashboardTest/Model/Dao/NewRelicDaoTest.php
+++ module/Dashboard/test/unit/DashboardTest/Model/Dao/NewRelicDaoTest.php
@@ -17,10 +17,10 @@ class NewRelicDaoTest extends AbstractDaoTestCase
         $dao = Bootstrap::getServiceManager()->get('NewRelicDao');
         $dao->setDaoOptions(array(
             'headers' => array(
-                'x-api-key' => '0116c7512e1efa28a39116312e9640edb90f1f52bb6ab30',
+                'x-api-key' => 'foobar',
             ),
             'params' => array(
-                'accountId' => '100366',
+                'accountId' => '1111111',
             ),
         ));
 
@@ -37,7 +37,7 @@ class NewRelicDaoTest extends AbstractDaoTestCase
         $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
 
         $response = $this->testedDao->fetchRpmForNumberWidget(array(
-            'appId' => '1290733',
+            'appId' => '1111111',
         ));
 
         $this->assertInternalType('numeric', $response);
@@ -60,13 +60,213 @@ class NewRelicDaoTest extends AbstractDaoTestCase
     }
 
     /**
+     * Testing proper Jenkins API method - should return JSON parsed into array
+     *
+     * @dataProvider fetchFeRpmForNumberWidgetDataProvider
+     */
+    public function testFetchFeRpmForNumberWidget($apiResponse, $expectedDaoResponse)
+    {
+        $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
+
+        $response = $this->testedDao->fetchFeRpmForNumberWidget(array(
+            'appId' => '1111111',
+        ));
+
+        $this->assertInternalType('numeric', $response);
+        $this->assertEquals($expectedDaoResponse, $response);
+    }
+
+    /**
+     * Data provider for testFetchFeRpmForNumberWidget()
+     * @return array
+     */
+    public function fetchFeRpmForNumberWidgetDataProvider()
+    {
+        return [
+            'proper result' => [
+                '$apiResponse' =>
+                    __DIR__ . '/../../Mock/Dao/NewRelic/fetchFeRpmForNumberWidgetResponse.txt',
+                '$expectedDaoResponse' => 7908.0,
+            ],
+        ];
+    }
+
+    /**
+     * Testing proper Jenkins API method - should return JSON parsed into array
+     *
+     * @dataProvider fetchErrorRateForErrorWidgetDataProvider
+     */
+    public function testFetchErrorRateForErrorWidget($apiResponse, $expectedDaoResponse)
+    {
+        $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
+
+        $response = $this->testedDao->fetchErrorRateForErrorWidget(array(
+            'appId' => '1111111',
+        ));
+
+        $this->assertInternalType('numeric', $response);
+        $this->assertEquals($expectedDaoResponse, $response);
+    }
+
+    /**
+     * Data provider for testFetchErrorRateForErrorWidget()
+     * @return array
+     */
+    public function fetchErrorRateForErrorWidgetDataProvider()
+    {
+        return [
+            'proper result' => [
+                '$apiResponse' =>
+                    __DIR__ . '/../../Mock/Dao/NewRelic/fetchThresholdValuesResponse.txt',
+                '$expectedDaoResponse' => 0,
+            ],
+        ];
+    }
+
+
+    /**
+     * Testing proper Jenkins API method - should return JSON parsed into array
+     *
+     * @dataProvider fetchApdexForNumberWidgetDataProvider
+     */
+    public function testFetchApdexForNumberWidget($apiResponse, $expectedDaoResponse)
+    {
+        $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
+
+        $response = $this->testedDao->fetchApdexForNumberWidget(array(
+            'appId' => '1111111',
+        ));
+
+        $this->assertInternalType('numeric', $response);
+        $this->assertEquals($expectedDaoResponse, $response);
+    }
+
+    /**
+     * Data provider for testFetchErrorRateForErrorWidget()
+     * @return array
+     */
+    public function fetchApdexForNumberWidgetDataProvider()
+    {
+        return [
+            'proper result' => [
+                '$apiResponse' =>
+                    __DIR__ . '/../../Mock/Dao/NewRelic/fetchThresholdValuesResponse.txt',
+                '$expectedDaoResponse' => 0.99,
+            ],
+        ];
+    }
+
+    /**
+     * Testing proper Jenkins API method - should return JSON parsed into array
+     *
+     * @dataProvider fetchApdexForIncrementalGraphWidgetDataProvider
+     */
+    public function testFetchApdexForIncrementalGraphWidget($apiResponse, $expectedDaoResponse)
+    {
+        $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
+
+        $response = $this->testedDao->fetchApdexForIncrementalGraphWidget(array(
+            'appId' => '1111111',
+        ));
+
+        $this->assertInternalType('array', $response);
+        $this->assertEquals($expectedDaoResponse, $response);
+    }
+
+    /**
+     * Data provider for testFetchErrorRateForErrorWidget()
+     * @return array
+     */
+    public function fetchApdexForIncrementalGraphWidgetDataProvider()
+    {
+        return [
+            'proper result' => [
+                '$apiResponse' =>
+                    __DIR__ . '/../../Mock/Dao/NewRelic/fetchThresholdValuesResponse.txt',
+                '$expectedDaoResponse' => [
+                    'x' => 1401270766,
+                    'y' => "0.99",
+                    'events' => [],
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * Testing proper Jenkins API method - should return JSON parsed into array
+     *
+     * @dataProvider fetchCpuUsageForNumberWidgetDataProvider
+     */
+    public function testFetchCpuUsageForNumberWidget($apiResponse, $expectedDaoResponse)
+    {
+        $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
+
+        $response = $this->testedDao->fetchCpuUsageForNumberWidget(array(
+            'appId' => '1111111',
+        ));
+
+        $this->assertInternalType('numeric', $response);
+        $this->assertEquals($expectedDaoResponse, $response);
+    }
+
+    /**
+     * Data provider for testFetchErrorRateForErrorWidget()
+     * @return array
+     */
+    public function fetchCpuUsageForNumberWidgetDataProvider()
+    {
+        return [
+            'proper result' => [
+                '$apiResponse' =>
+                    __DIR__ . '/../../Mock/Dao/NewRelic/fetchCpuUsageForNumberWidgetResponse.txt',
+                '$expectedDaoResponse' => 26.8,
+            ],
+        ];
+    }
+
+    /**
+     * Testing proper Jenkins API method - should return JSON parsed into array
+     *
+     * @dataProvider fetchAverageResponseTimeForNumberWidgetDataProvider
+     */
+    public function testFetchAverageResponseTimeForNumberWidget($apiResponse, $expectedDaoResponse)
+    {
+        $this->testedDao->getDataProvider()->getAdapter()->setResponse(file_get_contents($apiResponse));
+
+        $response = $this->testedDao->fetchAverageResponseTimeForNumberWidget(array(
+            'appId' => '1111111',
+        ));
+
+        $this->assertInternalType('numeric', $response);
+        $this->assertEquals($expectedDaoResponse, $response);
+    }
+
+    /**
+     * Data provider for testFetchErrorRateForErrorWidget()
+     * @return array
+     */
+    public function fetchAverageResponseTimeForNumberWidgetDataProvider()
+    {
+        return [
+            'proper result' => [
+                '$apiResponse' =>
+                    __DIR__ . '/../../Mock/Dao/NewRelic/fetchAverageResponseTimeForNumberWidgetResponse.txt',
+                '$expectedDaoResponse' => 135.0,
+            ],
+        ];
+    }
+
+//
+
+
+    /**
      * Executing fetch* method that is not defined in JenkinsDao - should throw an Exception
      * @expectedException \Dashboard\Model\Dao\Exception\FetchNotImplemented
      */
     public function testImproperApiMethod()
     {
         $this->testedDao->fetchImproperDataName(array(
-            'appId' => '1290733',
+            'appId' => '1111111',
         ));
     }
 
