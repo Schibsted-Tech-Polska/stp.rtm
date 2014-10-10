974ff9b488f0e8eb29cf98bf8058503aac0f7510
Michal Plachta <michal.plachta@schibsted.pl>
2014-10-10 15:59:36 +0200

    Delete diet files from module. Whole configuration should be included in config dir.

--- module/Dashboard/config/autoload/dao/SmogDao.config.php
+++ module/Dashboard/config/autoload/dao/SmogDao.config.php
@@ -9,13 +9,13 @@ return [
             'fetchForSmogWidget' => 'http://monitoring.krakow.pios.gov.pl/iseo/aktualne_stacja.php?stacja=005',
         ],
         'norms' => [
-            'CO'	=> 10000,
-            'NO'	=> null,
-            'NO2'	=> 200,
-            'NOX'	=> 30,
-            'PM10'	=> 50,
-            'PM2.5'	=> 25,
-            'SO2'	=> 350,
+            'CO' => 10000,
+            'NO' => null,
+            'NO2' => 200,
+            'NOX' => 30,
+            'PM10' => 50,
+            'PM2.5' => 25,
+            'SO2' => 350,
         ]
     ],
 ];
