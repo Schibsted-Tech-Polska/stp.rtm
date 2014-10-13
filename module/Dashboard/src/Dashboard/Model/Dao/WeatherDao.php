779cdb597032f80a462238c0c11a45e7990000a2
Michal Plachta <michal.plachta@schibsted.pl>
2014-10-13 09:59:58 +0200

    Weather Widget

--- module/Dashboard/src/Dashboard/Model/Dao/WeatherDao.php
+++ module/Dashboard/src/Dashboard/Model/Dao/WeatherDao.php
@@ -7,11 +7,6 @@ class WeatherDao extends AbstractDao
 
     public function fetchForWeatherWidget(array $params)
     {
-        return $this->fetchWeather($params);
-    }
-
-    private function fetchWeather($params)
-    {
         return $this->request($this->getEndpointUrl(__FUNCTION__), $params);
     }
-} 
+}
--- module/Dashboard/test/unit/DashboardTest/Model/Dao/WeatherDaoTest.php
+++ module/Dashboard/test/unit/DashboardTest/Model/Dao/WeatherDaoTest.php
@@ -3,18 +3,32 @@
 namespace DashboardTest\Model\Dao;
 
 use Dashboard\Model\Dao\WeatherDao;
+use Zend\Uri\Uri;
 
-class WeatherDaoTest extends AbstractDaoTestCase
+class WeatherDaoTest extends \PHPUnit_Framework_TestCase
 {
     public function testFetchForWeatherWidget()
     {
+        $uri = new Uri();
         $params = [];
+        $result = [];
 
-        $observer = $this->getMock(WeatherDao::class, ['fetchWeather']);
+        $observer = $this->getMockBuilder(WeatherDao::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['request', 'getEndpointUrl'])
+            ->setMockClassName('WeatherDao')
+            ->getMock();
+
         $observer->expects($this->once())
-            ->method('fetchWeather')
-            ->with($this->exactly($params));
+            ->method('getEndpointUrl')
+            ->with('fetchForWeatherWidget')
+            ->willReturn($uri);
 
-//        $observer->fetchForWeatherWidget($params);
+        $observer->expects($this->once())
+            ->method('request')
+            ->with($uri, $params)
+            ->willReturn($result);
+
+        $this->assertEquals($result, $observer->fetchForWeatherWidget($params));
     }
 }
